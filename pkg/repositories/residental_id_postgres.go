dd9eb2d68e8135673d6fe98d02984b0aa6896a28
KazakhAttila <anuar.kuanysheev@gmail.com>
2023-07-04 02:56:46 +0600

    Update residental_id_postgres.go

--- pkg/repositories/residental_id_postgres.go
+++ pkg/repositories/residental_id_postgres.go
@@ -32,22 +32,4 @@ func (r *ResidentSlugPostgres) Refresh(slug string) (resident.ResidentSlug, erro
 }
 
 /*
-time out suka: 
-right now task is -> this or other random approahes way -> just to get to the main ...
-get the services, repos and handlers work together in real time, just as the fucking main... 
-
-then get to the FUCKING ZAPROSI AND FETCHING THEM REAL TIME!!! 
-also CHANNELS AND DOCKER! 
-tomorrow ASIK SOZVON!!!! 
-
-current plan and what was done -> little cosmetic changes and some slurs or some micro mistakes out there... pzdc
-pohui. prostim i zabudem? 
-pohui dal'we same tempo + some new microinfos
-
-koroche was -> VSyu chernuyu rabotu. 
-dazhe channels nahooi dolzhen bit' zdes' -> status of the request, pohui nahui! 
-repos ideal'no. 
-dal'we -> services ideal'no. 
-dal'we -> handlers tozhe ideal'no suka. 
-
 */
